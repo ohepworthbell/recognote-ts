b3d5afda24c512d886dd2c4f2b92a37f5a4ebde2
Oliver <oliver.hepworth@googlemail.com>
2020-11-27 23:58:41 +0000

    feat:scaffold

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: ['./src/index.ts', './src/assets/scss/style.scss'],
+  plugins: [new miniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?[ac]?ss$/i,
+        use: [miniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000
+  },
+  watch: true
+};
